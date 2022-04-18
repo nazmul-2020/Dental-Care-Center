a49fa065e467255ee7fa26e2abfc579fe06b9f61
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-04-18 20:18:38 +0600

    update about

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import about from '../../images/pavel-removebg-preview.png'
 
 const About = () => {
     return (
-        <div>
-            <h3>this is about </h3>
+        <div className='container mt-3'>
+            <h3 className='text-center '>ABOUT ME</h3>
+            <div className=' row g-5 my-5  border border-success  shadow rounded'>
+                <div className=' col-lg-6 col-md-12 d-flex d-flex align-items-center text-justify '>
+                    <div>
+                    <h3 className='text-center text-primary pt-3'>My Mission</h3>
+                    <p> Hi! This is  Nazmul Hasan. Now! am really hardworking to become a
+                        successful Web Developer. After 6 months I want to make
+                        myself a professional Web Developer. For this reason, I am working 6-8 hours a day after mine.</p>
+                    </div>
+                </div>
+                <div className='col-lg-6 col-md-12 px-5 mx-auto d-block'>
+                    <img className='w-75 ' src={about} alt="" />
+
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -7,48 +7,35 @@ const Blog = () => {
             <h2> My Blog</h2>
             <Accordion className=' p-3 rounded shadow-lg ' >
                 <Accordion.Item eventKey="0">
-                    <Accordion.Header>Difference between authorization and authentication Authentication</Accordion.Header>
+                    <Accordion.Header className='text-start'>Difference between authorization and authentication Authentication</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        <h5>Authentication</h5>
+                        <li> Authentication verifies who the user is.</li>
+                        <li> Authentication works through passwords, one-time pins, biometric information, and other information provided or entered by the user.</li>
+                        <li>Authentication is the first step of a good identity and access management process.</li>
+                        <h5>Authorization</h5>
+                        <li>Authorization determines what resources a user can access.</li>
+                        <li>Authorization works through settings that are implemented and maintained by the organization.</li>
+                        <li>Authorization always takes place after authentication.</li>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
-                    <Accordion.Header>why are you using firebase? what other options do you have to implement authentication?</Accordion.Header>
+                    <Accordion.Header>Why are you using firebase? what other options do you have to implement authentication?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        <p>Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made Ul libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.</p>
                     </Accordion.Body>
                 </Accordion.Item>
-                <Accordion.Item eventKey="1">
+                <Accordion.Item eventKey="2">
                     <Accordion.Header>What other services does firebase provide other than authentication</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        <li>Cloud Firestore.</li>
+                        <li>Cloud Functions.</li>
+                        <li>Hosting.</li>
+                        <li>Google Analytics.</li>
+                        <li>Cloud Messaging.</li>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
-
-
-
-            <br /><br />
-            <br />
-
         </div>
     );
 };
