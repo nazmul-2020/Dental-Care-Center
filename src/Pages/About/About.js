6174a85762796cee9f26be6651ff65e2a97652fb
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-04-18 22:23:06 +0600

    Eidet ReadME file

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <div className=' row g-5 my-5  border border-success  shadow rounded'>
                 <div className=' col-lg-6 col-md-12 d-flex d-flex align-items-center text-justify '>
                     <div>
-                    <h3 className='text-center text-primary pt-3'>My Mission</h3>
+                    <h3 className='text-center text-primary pt-3'>My Goal</h3>
                     <p> Hi! This is  Nazmul Hasan. Now! am really hardworking to become a
                         successful Web Developer. After 6 months I want to make
                         myself a professional Web Developer. For this reason, I am working 6-8 hours a day after mine.</p>
